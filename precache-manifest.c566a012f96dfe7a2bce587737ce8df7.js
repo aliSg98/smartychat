734903b3e8c87b10b5a485494656c38d93c0006c
[Ali] <[ali_sg@outlook.com]>
2022-04-08 23:22:31 +0200

    Updates

---
+++ precache-manifest.c566a012f96dfe7a2bce587737ce8df7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "027a40fe9751b5df846d7cc8d44f7417",
+    "url": "/smartychat/index.html"
+  },
+  {
+    "revision": "b449d5cfaa4ad56e42fa",
+    "url": "/smartychat/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "12390b6768f9126e57a4",
+    "url": "/smartychat/static/js/2.59abb933.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/smartychat/static/js/2.59abb933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b449d5cfaa4ad56e42fa",
+    "url": "/smartychat/static/js/main.cfa15c07.chunk.js"
+  },
+  {
+    "revision": "3782eb508e847ad8f5c4",
+    "url": "/smartychat/static/js/runtime-main.6b60ae00.js"
+  }
+]);
